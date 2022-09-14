6adf702c4df2f2da26fbba1c576e6e6e989ff47f
anjalidalal <anjalimaria2002@gmail.com>
2022-09-14 20:05:33 +0530

    stone-paper-scissors game

---
+++ index.js
@@ -0,0 +1,48 @@
+const main = document.querySelector(".main");
+const secondPage = document.querySelector(".second-page");
+const yPlayer = document.querySelector(".you");
+const cPlayer = document.querySelector(".computer");
+const yourScore = document.querySelector("you-score");
+const computerScore = document.querySelector("computer-score");
+
+const buttons = document.querySelectorAll(".button-section button");
+
+const stone = document.createElement("img");
+stone.src = "./rock.svg";
+stone.classList.add("stone-style");
+
+const paper = document.createElement("img");
+paper.src = "./paper.svg";
+paper.classList.add("paper-style");
+
+const scissors = document.createElement("img");
+scissors.src = "./scissors.svg";
+scissors.classList.add("scissors-style");
+
+let randomArray = [stone.cloneNode(), paper.cloneNode(), scissors.cloneNode()];
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let randomNum = Math.floor(Math.random() * randomArray.length);
+
+    cPlayer.innerHTML = "";
+    cPlayer.appendChild(randomArray[randomNum]);
+    if (e.target.className === "scissors") {
+      yPlayer.innerHTML = "";
+      yPlayer.appendChild(scissors);
+    }
+    if (e.target.className === "stone") {
+      yPlayer.innerHTML = "";
+      yPlayer.appendChild(stone);
+    }
+    if (e.target.className === "paper") {
+      yPlayer.innerHTML = "";
+      yPlayer.appendChild(paper);
+    }
+
+    console.log(yPlayer.innerHTML === cPlayer.innerHTML);
+    if (yPlayer.innerHTML === cPlayer.innerHTML) {
+      console.log("match is tie");
+    }
+  });
+});
