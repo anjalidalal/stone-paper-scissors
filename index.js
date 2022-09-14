3a1caa02754859a8e41e173ead52c8f966012c18
anjalidalal <anjalimaria2002@gmail.com>
2022-09-15 01:20:23 +0530

    stone-paper-scissors game

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const gameState = document.querySelector(".state-of-game");
 const playAgain = document.getElementById("play-again");
 const yPlayer = document.querySelector(".you");
 const cPlayer = document.querySelector(".computer");
-const yourScore = document.querySelector("you-score");
-const computerScore = document.querySelector("computer-score");
 
 const buttons = document.querySelectorAll(".button-section button");
 
@@ -27,38 +25,118 @@ buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     let randomNum = Math.floor(Math.random() * randomArray.length);
 
-    cPlayer.innerHTML = "";
-    cPlayer.appendChild(randomArray[randomNum]);
     if (e.target.className === "scissors") {
       yPlayer.innerHTML = "";
       yPlayer.appendChild(scissors);
+      cPlayer.innerHTML = "";
+      cPlayer.appendChild(randomArray[randomNum]);
     }
     if (e.target.className === "stone") {
       yPlayer.innerHTML = "";
       yPlayer.appendChild(stone);
+      cPlayer.innerHTML = "";
+      cPlayer.appendChild(randomArray[randomNum]);
     }
     if (e.target.className === "paper") {
       yPlayer.innerHTML = "";
       yPlayer.appendChild(paper);
+      cPlayer.innerHTML = "";
+      cPlayer.appendChild(randomArray[randomNum]);
     }
 
-    //console.log(yPlayer.innerHTML === cPlayer.innerHTML);
-
-    if (
-      yPlayer.innerHTML === cPlayer.innerHTML &&
-      yPlayer.innerHTML !== "" &&
-      cPlayer.innerHTML !== ""
-    ) {
-      setTimeout(() => {
-        main.style.display = "none";
-        gameState.innerHTML = "Match is Tie !";
-        secondPage.classList.add("second-page-style");
-      }, 1000);
-      console.log("match is tie", true);
-    }
+    tieMatch();
+    yourWinCondition();
+    computerWinConditions();
   });
 });
 
 playAgain.addEventListener("click", () => {
   location.reload();
 });
+
+function yourWinCondition() {
+  if (
+    yPlayer.children[0].classList.contains("stone-style") &&
+    cPlayer.children[0].classList.contains("paper-style")
+  ) {
+    setTimeout(() => {
+      main.style.display = "none";
+      gameState.innerHTML = "YOU WIN🎉";
+      secondPage.classList.add("second-page-style");
+    }, 1000);
+  } else if (
+    yPlayer.children[0].classList.contains("scissors-style") &&
+    cPlayer.children[0].classList.contains("paper-style")
+  ) {
+    setTimeout(() => {
+      main.style.display = "none";
+      gameState.innerHTML = "YOU WIN🎉";
+      secondPage.classList.add("second-page-style");
+    }, 1000);
+  } else if (
+    yPlayer.children[0].classList.contains("stone-style") &&
+    cPlayer.children[0].classList.contains("scissors-style")
+  ) {
+    setTimeout(() => {
+      main.style.display = "none";
+      gameState.innerHTML = "YOU WIN🎉";
+      secondPage.classList.add("second-page-style");
+    }, 1000);
+  }
+}
+
+function computerWinConditions() {
+  if (
+    yPlayer.children[0].classList.contains("scissors-style") &&
+    cPlayer.children[0].classList.contains("stone-style")
+  ) {
+    setTimeout(() => {
+      main.style.display = "none";
+      gameState.innerHTML = "COMPUTER WIN🎉";
+      secondPage.classList.add("second-page-style");
+    }, 1000);
+  } else if (
+    yPlayer.children[0].classList.contains("paper-style") &&
+    cPlayer.children[0].classList.contains("scissors-style")
+  ) {
+    setTimeout(() => {
+      main.style.display = "none";
+      gameState.innerHTML = "COMPUTER WIN🎉";
+      secondPage.classList.add("second-page-style");
+    }, 1000);
+  } else if (
+    yPlayer.children[0].classList.contains("paper-style") &&
+    cPlayer.children[0].classList.contains("stone-style")
+  ) {
+    setTimeout(() => {
+      main.style.display = "none";
+      gameState.innerHTML = "COMPUTER WIN🎉";
+      secondPage.classList.add("second-page-style");
+    }, 1000);
+  }
+}
+
+function tieMatch() {
+  if (
+    yPlayer.innerHTML === cPlayer.innerHTML &&
+    yPlayer.innerHTML !== "" &&
+    cPlayer.innerHTML !== ""
+  ) {
+    setTimeout(() => {
+      main.style.display = "none";
+      gameState.innerHTML = "Match is Tie !";
+      secondPage.classList.add("second-page-style");
+    }, 1000);
+  }
+}
+
+//  Y         C
+// stone      scissor    y++
+// paper      scissor    c++
+// scissor    scissor    tie
+// stone      paper      y++
+// paper      paper      tie
+// scissor    paper      y++
+// stone      stone      tie
+// paper      stone      c++
+// scissor    stone      c++
